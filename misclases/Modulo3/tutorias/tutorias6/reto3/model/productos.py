f90efd1b163dc2b736b01c5317225e3d5681352d
José Balbuena <45837292+JoseBalbuena181096@users.noreply.github.com>
2025-02-19 00:04:22 -0600

    tutoria 6 modulo 3

---
+++ misclases/Modulo3/tutorias/tutorias6/reto3/model/productos.py
@@ -0,0 +1,20 @@
+
+productos = {
+    1: {
+        "nombre" : "manzana",
+        "precio" : 1.5,
+        "stock" : 100
+    },
+    2: {
+        "nombre" : "pera",
+        "precio" : 2,
+        "stock" : 100
+    },
+    3: {
+        "nombre" : "sandia",
+        "precio" : 3,
+        "stock" : 100
+    }
+}
+
+historial_ventas = []
